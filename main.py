7e8d424f734a3c0fb5622d4c561a2f57aae64900
Sergii Semenets <serhey.semenets@gmail.com>
2024-12-09 10:55:43 +0200

    add users & cars to main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from routers import users, cars
 
 app = FastAPI()
+
+app.include_router(users.router, prefix="/users", tags=["Users"])
+app.include_router(cars.router, prefix="/cars", tags=["Cars"])
 
 
 @app.get("/")
