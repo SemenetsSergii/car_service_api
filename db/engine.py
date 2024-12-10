f1bb0edd991f2ff1b0978e7bdbb6304d26cf4a81
Sergii Semenets <serhey.semenets@gmail.com>
2024-12-10 15:39:06 +0200

    add change db

--- db/engine.py
+++ db/engine.py
@@ -1,7 +1,18 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./my_test.db"
+# Load environment variables
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+SQLALCHEMY_DATABASE_URL = f"mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, future=True, echo=True)
 
